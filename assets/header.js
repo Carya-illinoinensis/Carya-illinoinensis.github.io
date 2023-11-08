a0a2669f5736b8d9c41d86ab7efb3dbb856dc15a
C-i <brent.d.burgess@gmail.com>
2023-11-07 22:03:57 -0800

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -1,5 +1,5 @@
-import { html } from "lighterhtml";
-import settings from "../settings.json";
+import { html } from "NL1";
+import settings from "./settings.json";
 
 export const renderHeader = html.node`<div>
   <div class='avatar-container'>
